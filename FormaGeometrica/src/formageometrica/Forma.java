e2d3d7d42d772574cf163e3b47c7685cf9456fb7
Filote Cosmin Catalin <filote_cosmin96@yahoo.com>
2017-04-10 20:00:34 +0300

    Add files via upload
    Inceput1

---
+++ FormaGeometrica/src/formageometrica/Forma.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formageometrica;
+
+/**
+ *
+ * @author Lenovo
+ */
+public interface Forma {
+    double Arie(double X,double Y);
+    double Perimetru(double X,double Y);
+    
+}
---
+++ FormaGeometrica/src/formageometrica/Patrat.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formageometrica;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Patrat implements Forma{
+
+    @Override
+    public double Arie(double X, double Y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double Perimetru(double X, double Y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
