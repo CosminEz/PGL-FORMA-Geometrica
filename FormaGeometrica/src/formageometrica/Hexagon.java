47c0a91cd825681ed15a94fc50b6ba12303c49b2
Cosmin <filote_cosmin96@yahoo.com>
2017-04-11 12:57:32 +0300

    Am adaugat clasa Hexagon ce implementeaza interfata Forma

---
+++ FormaGeometrica/src/formageometrica/Hexagon.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formageometrica;
+
+/**
+ *
+ * @author Cosmin
+ */
+public class Hexagon implements Forma{
+
+    @Override
+    public double Arie(double X, double Y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double Perimetru(double X, double Y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double Volum(double X, double Y, double Z) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
